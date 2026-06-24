7f6d08723a74b1a9fb7cb0a742635fc84283e27c
Mubashira <m49139010.@gmail.com>
2026-06-24 14:54:56 +0530

    Add Part1

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return <p>no feedback given</p>
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.total} />
+          <StatisticLine text="average" value={props.average} />
+          <StatisticLine text="positive" value={props.positive.toFixed(1) + " %"} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+export default App
