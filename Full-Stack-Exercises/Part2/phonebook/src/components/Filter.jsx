9f7c0842300583272551548f4e3ee2f06b52f500
Mubashira Suroor <m49139010@gmail.com>
2026-07-10 14:53:55 +0530

    Delete Full-Stack-Exercises directory

--- Full-Stack-Exercises/Part2/phonebook/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filter, onFilterChange }) => {
-  return (
-    <div>
-      filter shown with: <input value={filter} onChange={onFilterChange} />
-    </div>
-  )
-}
-
-export default Filter
